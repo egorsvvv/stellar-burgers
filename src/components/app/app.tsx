9d6d3248e75a5880e2639f3ee8aaadfb8ecf9a12
Egor Svintsitskii <egor_svvv@mail.ru>
2024-09-15 22:56:41 +0300

    first punct fifty fifty

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,14 +1,65 @@
-import { ConstructorPage } from '@pages';
+import {
+  ConstructorPage,
+  Login,
+  Feed,
+  Register,
+  ForgotPassword,
+  ResetPassword,
+  Profile,
+  ProfileOrders,
+  NotFound404
+} from '@pages';
 import '../../index.css';
 import styles from './app.module.css';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 
-import { AppHeader } from '@components';
+import { AppHeader, OrderInfo, Modal, IngredientDetails } from '@components';
+import ProtectedRoute from '../protected-route/protected-route';
 
 const App = () => (
-  <div className={styles.app}>
-    <AppHeader />
-    <ConstructorPage />
-  </div>
+  <BrowserRouter>
+    <div className={styles.app}>
+      <AppHeader />
+      <Routes>
+        <Route path='/' element={<ConstructorPage />} />
+        <Route path='/feed' element={<Feed />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/forgot-password' element={<ForgotPassword />} />
+        <Route element={<ProtectedRoute />}>
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/profile/orders' element={<ProfileOrders />} />
+          <Route path='/reset-password' element={<ResetPassword />} />
+          <Route
+            path='/profile/orders/:number'
+            element={
+              <Modal title='Детали заказа' onClose={() => {}}>
+                <OrderInfo />
+              </Modal>
+            }
+          />
+        </Route>
+        <Route path='/feed/:number' element={<OrderInfo />} />
+        <Route path='*' element={<NotFound404 />} />
+        <Route
+          path='/feed/:number'
+          element={
+            <Modal title='Детали заказа' onClose={() => {}}>
+              <OrderInfo />
+            </Modal>
+          }
+        />
+        <Route
+          path='/ingredients/:id'
+          element={
+            <Modal title='Детали заказа' onClose={() => {}}>
+              <IngredientDetails />
+            </Modal>
+          }
+        />
+      </Routes>
+    </div>
+  </BrowserRouter>
 );
 
 export default App;
---
+++ src/components/protected-route/protected-route.tsx
@@ -0,0 +1,8 @@
+import { Outlet, Navigate } from 'react-router-dom';
+
+const ProtectedRoute = () => {
+  const auth = true;
+  return auth ? <Outlet /> : <Navigate to='login' />;
+};
+
+export default ProtectedRoute;
